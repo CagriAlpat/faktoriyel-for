b0e1e99ca1d51149814f612e26ef1ce57821aaa6
CagriAlpat <cagri.alpat@gmail.com>
2023-08-06 17:55:01 +0300

    faktoriyel-for
    for

---
+++ 23for4.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 10; i++ {
+		faktoriyel := 1
+		for j := 1; j <= i; j++ {
+			faktoriyel *= j //faktoriyel= faktroiyal * j
+
+		}
+		fmt.Println(i, "!=", faktoriyel)
+	}
+
+}
